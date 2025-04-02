50e3178da3421302177507aecf7f765335d801a3
Zamauri Jones VT <zjones05@vt.edu>
2025-04-02 15:04:07 -0400

    changes

--- modelExperiment.R
+++ modelExperiment.R
@@ -31,27 +31,28 @@ plot(time_series, main = "Random Exponential Time Series (10,000 Points)",
 #----------FIT MODELS----------
 # Apply AR Model
 ar_model <- arima(time_series, order = c(1, 0, 0))
-print(ar_model)
-
-#AR acf/pacf
-acf(ar_model)
-pacf(ar_model)
+ar_model
 
 # Apply MA Model
 ma_model <- arima(time_series, order = c(0, 0, 1))
-print(ma_model)
+ma_model
 
 # Apply ARMA Model
 arma_model <- arima(time_series, order = c(1, 0, 1))
-print(arma_model)
+arma_model
 
 # Apply ARMA + Linear Trend Model
 arma_linear_model <- arima(time_series, order = c(1, 0, 1), xreg = 1:n)
-print(arma_linear_model)
+arma_linear_model
 
 # Apply Sine + Noise Model
 omega <- 0.05
 a <- 2
 sine_noise <- a * sin(omega * (1:n)) + rnorm(n)
 sin_noise_model <- arima(time_series, order = c(1, 0, 1), xreg = sine_noise)
-print(sin_noise_model)
+sin_noise_model
+
+#ma < ar < armaLinear < arma < sine
+#MA model has best fit
+
+#----------APPLYING TO TRAINING SET----------
